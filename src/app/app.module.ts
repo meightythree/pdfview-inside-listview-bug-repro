4814eebe958605f0093960b332e05bdca75c0234
palikollar <palkollar1995@gmail.com>
2022-06-22 21:00:44 +0200

    build: :heavy_plus_sign: Install nativescript-pdf-view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
 import { NativeScriptCommonModule, NativeScriptModule } from '@nativescript/angular'
+import { PDFViewModule } from 'nativescript-pdf-view/angular';
 
 import { AppComponent } from './app.component'
 
 @NgModule({
   bootstrap: [AppComponent],
-  imports: [NativeScriptModule, NativeScriptCommonModule],
+  imports: [NativeScriptModule, NativeScriptCommonModule, PDFViewModule],
   declarations: [AppComponent],
   providers: [],
   schemas: [NO_ERRORS_SCHEMA],
