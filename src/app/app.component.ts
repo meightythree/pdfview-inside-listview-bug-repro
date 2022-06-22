e826c76e6f6ff0a3e8b7efa6655c277df17bfea2
palikollar <palkollar1995@gmail.com>
2022-06-22 21:16:30 +0200

    feat: :memo: Reproduce bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core'
+import { ItemEventData, Screen } from '@nativescript/core';
+
+import { BASE64_PDFS } from './data'
 
 @Component({
   selector: 'ns-app',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly BASE64_PDFS = BASE64_PDFS.map((data: string) => `data:application/pdf;base64,${data}`);
+  readonly HEIGHT = Screen.mainScreen.heightDIPs;
+
+  onLoaded(): void {
+    console.log('onLoaded');
+  }
+
+  onItemTap(event: ItemEventData): void {
+    console.log('onItemTap', event.index);
+  }
+  
+  onItemLoading(event: ItemEventData): void {
+    console.log('onItemLoading', event.index);
+  }
+  
+  onLoad(index: number): void {
+    console.log('onLoad', index);
+  }
+}
